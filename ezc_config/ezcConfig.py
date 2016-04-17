f0884bb9687e66163d8800d0d4ce26bb42eebd43
cuikeqiang <cuikeqiang@qq.com>
2016-04-18 00:32:58 +0800

    Use config file

--- ezc_config/ezcConfig.py
+++ ezc_config/ezcConfig.py
@@ -2,14 +2,21 @@ import os, sys
 import ConfigParser
 
 class EZCConfig():
+    imgFileName = 'ezcqrcode.png'
+    configFileName = 'ezc_config.conf'
+    conf = ConfigParser.ConfigParser()
 
     @staticmethod
     def getUrlHost():
-        return 'http://www.baidu.com/'
+        EZCConfig.conf.read(EZCConfig.getConfigFile())
+        EZCConfig.urlHost = EZCConfig.conf.get("url", "host")
+        return EZCConfig.urlHost
 
     @staticmethod
     def getUrlPathStudent():
-        return 's'
+        EZCConfig.conf.read(EZCConfig.getConfigFile())
+        EZCConfig.urlHost = EZCConfig.conf.get("url", "pathStudent")
+        return EZCConfig.urlHost
 
     @staticmethod
     def getTimeInterval():
@@ -18,11 +25,16 @@ class EZCConfig():
     @staticmethod
     def getImgPath():
         scriptDir = EZCConfig.getScriptDir()
-        imgPath = scriptDir + os.sep + 'ezcqrcode.png'
+        imgPath = scriptDir + os.sep + EZCConfig.imgFileName
         return imgPath
 
+    @staticmethod
+    def getConfigFile():
+        scriptDir = EZCConfig.getScriptDir()
+        configFile = scriptDir + os.sep + EZCConfig.configFileName
+        return configFile
+ 
     @staticmethod
     def getScriptDir():
         scriptDir = os.path.split(os.path.realpath(sys.argv[0]))[0]
         return scriptDir
-
